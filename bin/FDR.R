8c7d67b7e3a59c509440b4865ab3c9cd0e9c7a97
Adam Nunn <adam.nunn@ecseq.com>
2020-03-04 19:18:25 +0100

    minor fix

--- bin/FDR.R
+++ bin/FDR.R
@@ -7,6 +7,6 @@ args <- commandArgs(trailingOnly=T)
 model <- read.table(args[1],header=F)
 colnames(model) <- c("snp", "cpg", "beta", "stats", "pvalue")
 
-model$FDR <- p.adjust(p = model[-1,5], method = "BH", n = length(model[-1,5]))
+model$FDR <- p.adjust(p = model[,5], method = "BH", n = length(model[,5]))
 
 write.table(model[,c("cpg","snp","beta","stats","pvalue","FDR")], args[2], sep = "\t", row.names = FALSE, quote = FALSE)
