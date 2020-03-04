04f192c4e39c91b8c7976f1deb46e74e010f4251
Adam Nunn <adam.nunn@ecseq.com>
2020-03-04 15:29:28 +0100

    typo 13 14

--- bin/GEM_Gmodel.R
+++ bin/GEM_Gmodel.R
@@ -10,8 +10,8 @@ suppressMessages(require(usethis))
 
 ##################################
 args <- commandArgs(trailingOnly=T)
-source(paste(args[1],"GEM_model.R",sep="/")
-source(paste(args[1],"Matrix_eQTL_engine3.R",sep="/")
+source(paste(args[1],"GEM_model.R",sep="/"))
+source(paste(args[1],"Matrix_eQTL_engine3.R",sep="/"))
 environment(my.GEM_Gmodel) <- asNamespace("GEM")
 environment(Matrix_eQTL_engine3) <- asNamespace("GEM")
 ##################################
