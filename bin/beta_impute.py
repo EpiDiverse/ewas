019fa5d725c1ddc6b720075bd5d538627663a856
Nilay Can <nilaycan@biologie.uni-marburg.de>
2020-06-30 17:02:09 +0200

    Update beta_impute.py

--- bin/beta_impute.py
+++ bin/beta_impute.py
@@ -19,8 +19,8 @@ import numpy as np
 import argparse
 
 argparser = argparse.ArgumentParser()
-argparser.add_argument('--filter_NA', type="double")
-argparser.add_argument('--filter_SD', type="double")
+argparser.add_argument('--filter_NA', type=int)
+argparser.add_argument('--filter_SD', type=int)
 ARGS = argparser.parse_args()
 
 print(ARGS.text)
