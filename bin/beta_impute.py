fe23fccf399b21de0bcdb73ec37633ce0257ab89
Nilay Can <nilaycan@biologie.uni-marburg.de>
2020-07-01 12:15:22 +0200

    Update beta_impute.py

--- bin/beta_impute.py
+++ bin/beta_impute.py
@@ -30,57 +30,57 @@ ARGS = argparser.parse_args()
 #DEFINE __MAIN__
 
 def main(fin,fout):
-	
-	#1) Open the unfiltered file for reading and writing
-	with open(sys.argv[1], "r") as fin, open("beta_imputed_" + sys.argv[1], "w") as fout:
-		
-		
-		## 2: Loop through lines with NAs < filter_NA.
-		line_num = 0
-		proc_lines = 0
-		for line in fin:
-			line = line.strip()
-			splitline = line.split("\t")
-			line_num += 1
-			pos = splitline[:3]
-			val = splitline[3:]
-			spls = len(val)
-			max_NA = spls * filter_NA                       #Calculate max number of NAs
-			
-			## 3: Calculate position average and st.dev
-			if line_num > 1 and line.count('NA') < max_NA:
-				v = [i for i in val if str(i) != 'NA']
-				v = [float(i) for i in v]
-				av = mean(v)
-				sd = pstdev(v)
-				
-				if sd > filter_SD:
-					proc_lines += 1
-					
-					
-					## 4: Fit the beta distribution (manual calculation of a and b parameters)
-					if 0.001 < av < 0.999:                  #Exclude positions with all zeros or all ones and sd = 0 (sd = 0 won't allow a and b calculation)
-						a = av**2 * ((1 - av) / sd**2 - (1 / av))
-						b = a * (1 / av - 1)
-						
-						## 5: Randomly sample values from the beta distribution with "numpy" and substitute them to NAs
-						for x in range(spls):
-							if val[x] == "NA":
-								val[x]= np.random.beta(a,b)
-								val[x]= '{:.2f}'.format(val[x])
-								
-								
-					elif av >= 0.999:                       #Impute positions with all "
-						for x in range(spls):
-							if val[x] == "NA":
-								val[x]= '{:.2f}'.format(1.00)
-					else:                                   #Impute positions with all "0s"
-						for x in range(spls):
-						if val[x] == "NA":
-							val[x]= '{:.2f}'.format(0.00)
-						pos.extend(val)                         #Append val list to pos list
-					print(*pos, sep="\t", file=fout)
-				elif line_num < 2:
+    
+    #1) Open the unfiltered file for reading and writing
+    with open(sys.argv[1], "r") as fin, open("beta_imputed_" + sys.argv[1], "w") as fout:
+        
+        
+        ## 2: Loop through lines with NAs < filter_NA.
+        line_num = 0
+        proc_lines = 0
+        for line in fin:
+            line = line.strip()
+            splitline = line.split("\t")
+            line_num += 1
+            pos = splitline[:3]
+            val = splitline[3:]
+            spls = len(val)
+            max_NA = spls * filter_NA                       #Calculate max number of NAs
+            
+            ## 3: Calculate position average and st.dev
+            if line_num > 1 and line.count('NA') < max_NA:
+                v = [i for i in val if str(i) != 'NA']
+                v = [float(i) for i in v]
+                av = mean(v)
+                sd = pstdev(v)
+                
+                if sd > filter_SD:
+                    proc_lines += 1
+                    
+                    
+                    ## 4: Fit the beta distribution (manual calculation of a and b parameters)
+                    if 0.001 < av < 0.999:                  #Exclude positions with all zeros or all ones and sd = 0 (sd = 0 won't allow a and b calculation)
+                        a = av**2 * ((1 - av) / sd**2 - (1 / av))
+                        b = a * (1 / av - 1)
+                        
+                        ## 5: Randomly sample values from the beta distribution with "numpy" and substitute them to NAs
+                        for x in range(spls):
+                            if val[x] == "NA":
+                                val[x]= np.random.beta(a,b)
+                                val[x]= '{:.2f}'.format(val[x])
+                                
+                                
+                    elif av >= 0.999:                       #Impute positions with all "
+                        for x in range(spls):
+                            if val[x] == "NA":
+                                val[x]= '{:.2f}'.format(1.00)
+                    else:                                   #Impute positions with all "0s"
+                        for x in range(spls):
+                        if val[x] == "NA":
+                            val[x]= '{:.2f}'.format(0.00)
+                        pos.extend(val)                         #Append val list to pos list
+                    print(*pos, sep="\t", file=fout)
+                elif line_num < 2:
 ## END OF _MAIN_
 
 # define argparse
@@ -93,7 +93,7 @@ parser.add_argument('outfile', metavar='out.bed', 'the path to the beta imputed 
 
 args= parser.parse_args()
 if __name__== '__main__':
-	main(args.infile,args.outfile)
+    main(args.infile,args.outfile)
 
 #print(line, sep="\t", file=fout)
 #fin.close()
@@ -101,3 +101,4 @@ if __name__== '__main__':
 
 #print(str(sys.argv[1]) + " contains " + str(spls) + " individulas")
 #print("Out of " + str(line_num) + " initial markers, " + str(proc_lines) + " were kept after filtering and imputation")
+
